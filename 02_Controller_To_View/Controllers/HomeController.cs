d05b3cdb5a2c4d92861ed3b462241cfddfc6786f
tcanpolat <thsn_cnplt@windowslive.com>
2026-08-29 12:35:22 +0300

    controller to view

---
+++ 02_Controller_To_View/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _02_Controller_To_View.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Controllerlardan View'e gitmek için action methodları kullanılır.
+        // Bu action methodları IActionResult tipinde olmalıdır.
+        // IActionResult tipinde olan methodlar View, Json, Html sayfası döndürebilir.
+        public IActionResult Index() // Bu method View/Home/Index.cshtml dosyasını geriye döndürür.
+        {
+            List<string> products = new List<string>() { "Ürün 1", "Ürün 2", "Ürün 3" };
+            // veriyi viewdata ile view'e gönderebiliriz.
+            ViewData["products"] = products;
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = $"Ürün {id}";
+            ViewData["productDetail"] = product;
+            return View();
+        }
+
+        
+    }
+}
